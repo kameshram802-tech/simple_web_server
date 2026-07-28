39a442079accde312992db363060f67dc8811a61
kameshram802-tech <kameshram802@gmail.com>
2026-07-28 14:04:37 +0530

    README.md

---
+++ exp-1.py
@@ -0,0 +1,15 @@
+from http.server import HTTPServer,BaseHTTPRequestHandler
+content='''<html>
+<h1>Hello Everyone!!</h1>
+</html>'''
+class Myserver(BaseHTTPRequestHandler):
+    def do_GET(self):
+        print("Get request recevied...")
+        self.send_response(200)
+        self.send_header("content-type","text/html")
+        self.end_headers()
+        self.wfile.write(content.encode())
+print("This is my webserver")
+server_address=('',8000)
+httpd=HTTPServer(server_address,Myserver)
+httpd.serve_forever()
